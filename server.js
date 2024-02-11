02b4e1073891f37356b5ba468bbbb3f6ef7f834f
ViveK <vivekartist9999@gmail.com>
2024-02-11 15:57:29 +0530

    init: project

---
+++ server.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+
+const app = require('./app');
+
+const port = process.env.PORT || 2727;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
